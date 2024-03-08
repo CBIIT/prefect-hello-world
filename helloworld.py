383c9f138247e0a80ca7cf34f91573383ecd9bcc
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-03-08 15:44:59 -0500

    added dask task runner

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,13 @@
+import dask
 from prefect import flow, task, get_run_logger
+from prefect_dask.task_runners import DaskTaskRunner
 
 @task
 def helloworld():
     return("Hello from Prefect.")
 
 
-@flow(name="Hello World")
+@flow(name="Hello World", task_runner=DaskTaskRunner(cluster_class="dask_cloudprovider.aws.FargateCluster",cluster_kwargs={"n_workers": 4, "threads_per_worker": 2}))
 def runner():
     logger = get_run_logger()
     logger.info(helloworld())
