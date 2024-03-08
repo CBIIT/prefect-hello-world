4656932d784fb3f8565ff4339a23e85f9d23c50e
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-03-08 14:01:21 -0500

    added run resources

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
 from prefect import flow, task, get_run_logger
+from prefect.run_configs import ECSRun
 
 @task
 def helloworld():
     return("Hello from Prefect.")
-    
 
-@flow(name="Hello World")
+
+@flow(name="Hello World", run_config=ECSRun(cpu="2 vcpu", memory="4 GB"))
 def runner():
     logger = get_run_logger()
     logger.info(helloworld())
